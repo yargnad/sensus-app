8c1800f088348bec5673bc899e54da617ec764ac
yargnad <yargnad@gmail.com>
2025-09-25 14:57:08 -0400

    build: Add production server logic

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,4 +25,14 @@ mongoose
 // Use Routes
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+}
+
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
